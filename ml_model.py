15c7841c3d04d4b56474812c881723b68aa9c338
tzuhsuan <55183489+tzuhsuan0410@users.noreply.github.com>
2024-12-30 23:36:32 +0000

    Update ml_model.py

--- ml_model.py
+++ ml_model.py
@@ -55,6 +55,8 @@ if uploaded_file:
     
     # 資料前處理
     data = preprocess_data(data)
+    num_rows = st.slider("選擇要顯示的筆數", min_value=1, max_value=len(data), value=5)
+    st.write(f"### 清理後的資料（顯示前 {num_rows} 筆）：", data.head(num_rows))
     st.write("### 清理後的資料：", data.head())
     
     # 特徵與標籤選擇
