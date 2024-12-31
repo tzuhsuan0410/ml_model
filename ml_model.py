7e83e0e83391c7ae7a2e1ece70427af75a0a071c
tzuhsuan <55183489+tzuhsuan0410@users.noreply.github.com>
2024-12-31 01:23:10 +0000

    Update ml_model.py

--- ml_model.py
+++ ml_model.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
